6da05890226ec4ff60c27d3c422b21e747c58f1a
liyuze <li-yz22@mails.tsinghua.edu.cn>
2023-02-24 17:37:19 +0800

    finish lab2

--- lab2/DebugExercise/DebugExercise1.java
+++ lab2/DebugExercise/DebugExercise1.java
@@ -6,9 +6,8 @@ package DebugExercise;
  */
 public class DebugExercise1 {
     public static int divideThenRound(int top, int bottom) {
-        int quotient = top / bottom;
-        int result = Math.round(quotient);
-        return result;
+        double quotient = 1.0*top / bottom;
+        return (int)Math.round(quotient);
     }
 
     public static void main(String[] args) {
--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -17,4 +17,12 @@ public class SquarePrimesTest {
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
         assertTrue(changed);
     }
+
+    @Test
+    public void testSquarePrimesOne() {
+        IntList lst = IntList.of(2, 3, 5);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("4 -> 9 -> 25", lst.toString());
+        assertTrue(changed);
+    }
 }
